1a1795b66c9f2e5c62f783c354c9be7910fbbe12
Ansh Mehta <8anshmehta@gmail.com>
2021-07-16 11:08:48 +0530

    removed unused functions

--- src/components/transfer/index.js
+++ src/components/transfer/index.js
@@ -1,4 +1,19 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+	Container,
+	Box,
+	Flex,
+	Text,
+	Center,
+	FormControl,
+	FormLabel,
+	Input,
+	FormHelperText,
+	Divider,
+	Button,
+} from '@chakra-ui/react';
+import toast from 'react-hot-toast';
 
 const Transfer = () => {
 	return <h1>Hello</h1>;
