bd68e71ebc0f3aa0e65935e5cba1453d0d4ee9d0
Ansh Mehta <8anshmehta@gmail.com>
2021-07-16 11:09:21 +0530

    removed unused libraries

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import {
 	Container,
@@ -16,16 +16,12 @@ import {
 import toast from 'react-hot-toast';
 
 import api from '../../api';
-import { AuthContext } from '../../context/AuthContext';
 
 const Home = props => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-	const [id, setId] = useState('');
-	const [balance, setBal] = useState('');
 
 	const history = useHistory();
-	const auth = useContext(AuthContext);
 
 	const onFormSubmit = async () => {
 		if (!email || !password) {
@@ -48,44 +44,42 @@ const Home = props => {
 	};
 
 	return (
-		<AuthContext.Provider value={{ id, balance }}>
-			<Flex justify="center" align="center" wrap="no-wrap">
-				<Container maxW="xl" mt="15vh" centerContent>
-					<Box padding="10" minWidth="30vw" bg="gray.50" borderRadius={10}>
-						<Center>
-							<Text fontSize="3xl">Login</Text>
-						</Center>
-						<Divider paddingTop="5" />
-						<Center>
-							<FormControl id="email" paddingY="5" isRequired>
-								<FormLabel>Email address</FormLabel>
-								<Input
-									value={email}
-									onChange={e => setEmail(e.target.value)}
-									placeholder="Type your e-mail address"
-									type="email"
-								/>
-								<FormHelperText>We'll never share your email.</FormHelperText>
-							</FormControl>
-						</Center>
-						<Center>
-							<FormControl id="password" paddingBottom="5" isRequired>
-								<FormLabel>Password</FormLabel>
-								<Input
-									value={password}
-									onChange={e => setPassword(e.target.value)}
-									placeholder="*********"
-									type="password"
-								/>
-							</FormControl>
-						</Center>
-						<Button onClick={onFormSubmit} colorScheme="blue">
-							Submit
-						</Button>
-					</Box>
-				</Container>
-			</Flex>
-		</AuthContext.Provider>
+		<Flex justify="center" align="center" wrap="no-wrap">
+			<Container maxW="xl" mt="15vh" centerContent>
+				<Box padding="10" minWidth="30vw" bg="gray.50" borderRadius={10}>
+					<Center>
+						<Text fontSize="3xl">Login</Text>
+					</Center>
+					<Divider paddingTop="5" />
+					<Center>
+						<FormControl id="email" paddingY="5" isRequired>
+							<FormLabel>Email address</FormLabel>
+							<Input
+								value={email}
+								onChange={e => setEmail(e.target.value)}
+								placeholder="Type your e-mail address"
+								type="email"
+							/>
+							<FormHelperText>We'll never share your email.</FormHelperText>
+						</FormControl>
+					</Center>
+					<Center>
+						<FormControl id="password" paddingBottom="5" isRequired>
+							<FormLabel>Password</FormLabel>
+							<Input
+								value={password}
+								onChange={e => setPassword(e.target.value)}
+								placeholder="*********"
+								type="password"
+							/>
+						</FormControl>
+					</Center>
+					<Button onClick={onFormSubmit} colorScheme="blue">
+						Submit
+					</Button>
+				</Box>
+			</Container>
+		</Flex>
 	);
 };
 
