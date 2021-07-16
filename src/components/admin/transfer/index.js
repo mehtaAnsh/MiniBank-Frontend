1cfc6182c7d08bd34c000fc1cadee2e70764f5b1
Ansh Mehta <8anshmehta@gmail.com>
2021-07-16 19:53:29 +0530

    added animation to cards

--- src/components/admin/transfer/index.js
+++ src/components/admin/transfer/index.js
@@ -10,19 +10,20 @@ import {
 	MenuButton,
 	Button,
 	MenuList,
-	FormHelperText,
 	Input,
 	RadioGroup,
 	Stack,
 	Radio,
+	FormControl,
+	MenuItem,
 } from '@chakra-ui/react';
 import { AiOutlineCaretDown } from 'react-icons/ai';
 import React, { useEffect, useState, useContext } from 'react';
 import toast from 'react-hot-toast';
 import { useHistory } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import { AuthContext } from '../../../context/AuthContext';
 import api from '../../../api';
-import { FormControl, MenuItem } from '@chakra-ui/react';
 
 const AdminTransfer = () => {
 	const auth = useContext(AuthContext);
@@ -79,62 +80,64 @@ const AdminTransfer = () => {
 	return (
 		<Flex justify="center" align="center" wrap="no-wrap">
 			<Container maxW="xl" mt="10vh" centerContent>
-				<Box padding="10" minWidth="30vw" bg="gray.50" borderRadius={10}>
-					<Center>
-						<Text fontSize="3xl">Login</Text>
-					</Center>
-					<Divider paddingTop="5" />
-					<Center>
-						<FormControl id="email" paddingY="5" isRequired>
-							<FormLabel>Select user ID</FormLabel>
-							<Menu>
-								<MenuButton as={Button} rightIcon={<AiOutlineCaretDown />}>
-									{id || 'Select one'}
-								</MenuButton>
-								<MenuList>
-									{dropdownArr.map(({ id }) => {
-										return (
-											<MenuItem
-												value={id}
-												onClick={e => {
-													setId(e.target.value);
-													console.log(e.target.value);
-												}}
-											>
-												{id}
-											</MenuItem>
-										);
-									})}
-								</MenuList>
-							</Menu>
-						</FormControl>
-					</Center>
-					<Center>
-						<FormControl isRequired paddingY="5">
-							<FormLabel>Type of transaction</FormLabel>
-							<RadioGroup onChange={setType} value={type}>
-								<Stack direction="row">
-									<Radio value="0">Debit</Radio>
-									<Radio value="1">Credit</Radio>
-								</Stack>
-							</RadioGroup>
-						</FormControl>
-					</Center>
-					<Center>
-						<FormControl id="password" paddingBottom="5" isRequired>
-							<FormLabel>Amount</FormLabel>
-							<Input
-								value={amt}
-								onChange={e => setAmt(e.target.value)}
-								placeholder="Enter amount to transfer"
-								type="number"
-							/>
-						</FormControl>
-					</Center>
-					<Button onClick={onFormSubmit} colorScheme="blue">
-						Submit
-					</Button>
-				</Box>
+				<motion.div animate={{ scale: 1.1 }} transition={{ duration: 0.5 }}>
+					<Box padding="10" minWidth="30vw" bg="gray.50" borderRadius={10}>
+						<Center>
+							<Text fontSize="3xl">Credit/Debit Money</Text>
+						</Center>
+						<Divider paddingTop="5" />
+						<Center>
+							<FormControl id="email" paddingY="5" isRequired>
+								<FormLabel>Select user ID</FormLabel>
+								<Menu>
+									<MenuButton as={Button} rightIcon={<AiOutlineCaretDown />}>
+										{id || 'Select one'}
+									</MenuButton>
+									<MenuList>
+										{dropdownArr.map(({ id }) => {
+											return (
+												<MenuItem
+													value={id}
+													onClick={e => {
+														setId(e.target.value);
+														console.log(e.target.value);
+													}}
+												>
+													{id}
+												</MenuItem>
+											);
+										})}
+									</MenuList>
+								</Menu>
+							</FormControl>
+						</Center>
+						<Center>
+							<FormControl isRequired paddingY="5">
+								<FormLabel>Type of transaction</FormLabel>
+								<RadioGroup onChange={setType} value={type}>
+									<Stack direction="row">
+										<Radio value="0">Debit</Radio>
+										<Radio value="1">Credit</Radio>
+									</Stack>
+								</RadioGroup>
+							</FormControl>
+						</Center>
+						<Center>
+							<FormControl id="password" paddingBottom="5" isRequired>
+								<FormLabel>Amount</FormLabel>
+								<Input
+									value={amt}
+									onChange={e => setAmt(e.target.value)}
+									placeholder="Enter amount to transfer"
+									type="number"
+								/>
+							</FormControl>
+						</Center>
+						<Button onClick={onFormSubmit} colorScheme="blue">
+							Submit
+						</Button>
+					</Box>
+				</motion.div>
 			</Container>
 		</Flex>
 	);
--- src/components/transfer/index.js
+++ src/components/transfer/index.js
@@ -52,39 +52,41 @@ const Transfer = () => {
 	return (
 		<Flex justify="center" align="center" wrap="no-wrap">
 			<Container maxW="xl" mt="15vh" centerContent>
-				<Box padding="10" minWidth="30vw" bg="gray.50" borderRadius={10}>
-					<Center>
-						<Text fontSize="3xl">Transfer Money</Text>
-					</Center>
-					<Divider paddingTop="5" />
-					<Center>
-						<FormControl id="" paddingY="5" isRequired>
-							<FormLabel>ID of the user to transfer</FormLabel>
-							<Input
-								value={id}
-								onChange={e => setId(e.target.value)}
-								placeholder="Type recepient's ID"
-								type="text"
-							/>
-							<FormHelperText>Make sure to enter correct ID of 16 length.</FormHelperText>
-							<FormHelperText>Add this to transfer: 6136533638928856 (for testing)</FormHelperText>
-						</FormControl>
-					</Center>
-					<Center>
-						<FormControl id="password" paddingBottom="5" isRequired>
-							<FormLabel>Amount to transfer</FormLabel>
-							<Input
-								value={amt}
-								onChange={e => setAmt(e.target.value)}
-								type="number"
-								placeholder="Enter amount to transfer"
-							/>
-						</FormControl>
-					</Center>
-					<Button onClick={onFormSubmit} colorScheme="blue">
-						Submit
-					</Button>
-				</Box>
+				<motion.div animate={{ scale: 1.1 }} transition={{ duration: 0.5 }}>
+					<Box padding="10" minWidth="30vw" bg="gray.50" borderRadius={10}>
+						<Center>
+							<Text fontSize="3xl">Transfer Money</Text>
+						</Center>
+						<Divider paddingTop="5" />
+						<Center>
+							<FormControl id="" paddingY="5" isRequired>
+								<FormLabel>ID of the user to transfer</FormLabel>
+								<Input
+									value={id}
+									onChange={e => setId(e.target.value)}
+									placeholder="Type recepient's ID"
+									type="text"
+								/>
+								<FormHelperText>Make sure to enter correct ID of 16 length.</FormHelperText>
+								<FormHelperText>Add this to transfer: 6136533638928856 (for testing)</FormHelperText>
+							</FormControl>
+						</Center>
+						<Center>
+							<FormControl id="password" paddingBottom="5" isRequired>
+								<FormLabel>Amount to transfer</FormLabel>
+								<Input
+									value={amt}
+									onChange={e => setAmt(e.target.value)}
+									type="number"
+									placeholder="Enter amount to transfer"
+								/>
+							</FormControl>
+						</Center>
+						<Button onClick={onFormSubmit} colorScheme="blue">
+							Submit
+						</Button>
+					</Box>
+				</motion.div>
 			</Container>
 		</Flex>
 	);
