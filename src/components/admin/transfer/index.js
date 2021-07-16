5f5d1ea2fc3ae6ae7cdc6e74bcfc9034338085c6
Ansh Mehta <8anshmehta@gmail.com>
2021-07-16 17:54:02 +0530

    all users and credit/debit implemented

--- src/components/admin/transfer/index.js
+++ src/components/admin/transfer/index.js
@@ -1,7 +1,143 @@
-import React from 'react';
+import {
+	Flex,
+	Container,
+	Box,
+	Center,
+	Divider,
+	Text,
+	FormLabel,
+	Menu,
+	MenuButton,
+	Button,
+	MenuList,
+	FormHelperText,
+	Input,
+	RadioGroup,
+	Stack,
+	Radio,
+} from '@chakra-ui/react';
+import { AiOutlineCaretDown } from 'react-icons/ai';
+import React, { useEffect, useState, useContext } from 'react';
+import toast from 'react-hot-toast';
+import { useHistory } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthContext';
+import api from '../../../api';
+import { FormControl, MenuItem } from '@chakra-ui/react';
 
 const AdminTransfer = () => {
-	return <h1>AdminTransfer</h1>;
+	const auth = useContext(AuthContext);
+	const history = useHistory();
+
+	const [dropdownArr, setDropDownArr] = useState([]);
+	const [id, setId] = useState('');
+	const [type, setType] = useState();
+	const [amt, setAmt] = useState();
+
+	useEffect(async () => {
+		if (!auth.isAdmin) {
+			history.push('/');
+			return;
+		}
+		var dropdownArr = [];
+		function mapAndFilter(arr) {
+			arr.forEach(obj => {
+				dropdownArr.push({ id: obj.id });
+			});
+			setDropDownArr(dropdownArr);
+		}
+		if (auth.usersObj.length === 0) {
+			await api
+				.get('/getUsers', {})
+				.then(res => {
+					auth.usersObj = res.data.users;
+					mapAndFilter(auth.usersObj);
+				})
+				.catch(err => toast.error('An error occured.'));
+			return;
+		}
+
+		mapAndFilter(auth.usersObj);
+	}, []);
+
+	const onFormSubmit = async () => {
+		if (!id || !type || !amt) {
+			toast.error('Fill all parameters');
+			return;
+		}
+		await api
+			.post('/transact', { id, type, amt })
+			.then(res => {
+				if (res.status === 201) {
+					toast.success('Money transferred sucessfully!');
+				} else {
+					toast.error('An unknown error occured. Please try again.');
+				}
+			})
+			.catch(err => toast.error(err.response.data.message));
+	};
+
+	return (
+		<Flex justify="center" align="center" wrap="no-wrap">
+			<Container maxW="xl" mt="10vh" centerContent>
+				<Box padding="10" minWidth="30vw" bg="gray.50" borderRadius={10}>
+					<Center>
+						<Text fontSize="3xl">Login</Text>
+					</Center>
+					<Divider paddingTop="5" />
+					<Center>
+						<FormControl id="email" paddingY="5" isRequired>
+							<FormLabel>Select user ID</FormLabel>
+							<Menu>
+								<MenuButton as={Button} rightIcon={<AiOutlineCaretDown />}>
+									{id || 'Select one'}
+								</MenuButton>
+								<MenuList>
+									{dropdownArr.map(({ id }) => {
+										return (
+											<MenuItem
+												value={id}
+												onClick={e => {
+													setId(e.target.value);
+													console.log(e.target.value);
+												}}
+											>
+												{id}
+											</MenuItem>
+										);
+									})}
+								</MenuList>
+							</Menu>
+						</FormControl>
+					</Center>
+					<Center>
+						<FormControl isRequired paddingY="5">
+							<FormLabel>Type of transaction</FormLabel>
+							<RadioGroup onChange={setType} value={type}>
+								<Stack direction="row">
+									<Radio value="0">Debit</Radio>
+									<Radio value="1">Credit</Radio>
+								</Stack>
+							</RadioGroup>
+						</FormControl>
+					</Center>
+					<Center>
+						<FormControl id="password" paddingBottom="5" isRequired>
+							<FormLabel>Amount</FormLabel>
+							<Input
+								value={amt}
+								onChange={e => setAmt(e.target.value)}
+								placeholder="Enter amount to transfer"
+								type="number"
+							/>
+						</FormControl>
+					</Center>
+					<Button onClick={onFormSubmit} colorScheme="blue">
+						Submit
+					</Button>
+				</Box>
+			</Container>
+		</Flex>
+	);
 };
 
 export default AdminTransfer;
--- src/components/admin/users/index.js
+++ src/components/admin/users/index.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import { Flex, Container, Box, Center, Divider, Text } from '@chakra-ui/layout';
+import React, { useEffect, useState, useContext } from 'react';
+import toast from 'react-hot-toast';
+import { useHistory } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthContext';
+import DataTable from 'react-data-table-component';
+import api from '../../../api';
 
 const AdminUsers = () => {
-	return <h1>AdminUsers</h1>;
+	const auth = useContext(AuthContext);
+	const history = useHistory();
+
+	const [users, setUsers] = useState([]);
+
+	useEffect(async () => {
+		if (!auth.isAdmin) {
+			history.push('/');
+			return;
+		}
+		if (auth.usersObj.length === 0) {
+			await api
+				.get('/getUsers', {})
+				.then(res => {
+					auth.usersObj = res.data.users;
+					setUsers(auth.usersObj);
+				})
+				.catch(err => toast.error('AN error occured.'));
+			return;
+		}
+		setUsers(auth.usersObj);
+	}, []);
+
+	const columns = [
+		{
+			name: 'User ID',
+			selector: 'id',
+		},
+		{
+			name: 'Email',
+			selector: 'email',
+		},
+		{
+			name: 'Balance',
+			selector: 'balance',
+			sortable: true,
+		},
+	];
+
+	return (
+		<Flex justify="center" align="center" wrap="no-wrap">
+			<Container maxW="xl" mt="5vh" centerContent>
+				<Box padding="10" minWidth="50vw" borderWidth="5" bg="#ffffff" borderRadius={10}>
+					<Center>
+						<Text fontSize="3xl">List of all Users</Text>
+					</Center>
+					<Divider paddingTop="10" />
+
+					<DataTable columns={columns} data={users} responsive pagination paginationPerPage="5" />
+				</Box>
+			</Container>
+		</Flex>
+	);
 };
 
 export default AdminUsers;
