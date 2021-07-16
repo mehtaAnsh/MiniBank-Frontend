569859d6081090da629c3d7029bc05db518f58b0
Ansh Mehta <8anshmehta@gmail.com>
2021-07-16 19:55:19 +0530

    added missing library

--- src/components/transfer/index.js
+++ src/components/transfer/index.js
@@ -14,6 +14,7 @@ import {
 	Button,
 } from '@chakra-ui/react';
 import toast from 'react-hot-toast';
+import { motion } from 'framer-motion';
 
 import api from '../../api';
 import { AuthContext } from '../../context/AuthContext';
