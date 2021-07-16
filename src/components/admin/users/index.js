94ada38f71903232ad7e4588fe4b1f40dc4fabb0
Ansh Mehta <8anshmehta@gmail.com>
2021-07-16 19:01:18 +0530

    add new user done

--- src/components/admin/users/index.js
+++ src/components/admin/users/index.js
@@ -1,16 +1,38 @@
 import { Flex, Container, Box, Center, Divider, Text } from '@chakra-ui/layout';
+import {
+	useDisclosure,
+	Drawer,
+	DrawerOverlay,
+	DrawerContent,
+	DrawerCloseButton,
+	DrawerHeader,
+	DrawerBody,
+	Stack,
+	FormLabel,
+	Input,
+	DrawerFooter,
+	Button,
+	FormControl,
+} from '@chakra-ui/react';
+import { GrAdd } from 'react-icons/gr';
 import React, { useEffect, useState, useContext } from 'react';
 import toast from 'react-hot-toast';
 import { useHistory } from 'react-router-dom';
 import { AuthContext } from '../../../context/AuthContext';
 import DataTable from 'react-data-table-component';
 import api from '../../../api';
+import { FormHelperText } from '@chakra-ui/react';
 
 const AdminUsers = () => {
 	const auth = useContext(AuthContext);
 	const history = useHistory();
 
 	const [users, setUsers] = useState([]);
+	const [email, setEmail] = useState();
+	const [bal, setBal] = useState();
+
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const firstField = React.useRef();
 
 	useEffect(async () => {
 		if (!auth.isAdmin) {
@@ -29,6 +51,31 @@ const AdminUsers = () => {
 		}
 		setUsers(auth.usersObj);
 	}, []);
+
+	const onFormSubmit = async () => {
+		if (!email || !bal) {
+			toast.error('Fill required parameters');
+			return;
+		}
+		await api
+			.post('/create_user', { email, balance: bal })
+			.then(async res => {
+				if (res.status === 201) {
+					toast.success('User added successfully! An e-mail has been sent with login credentials.');
+					onClose();
+					await api
+						.get('/getUsers', {})
+						.then(res => {
+							auth.usersObj = res.data.users;
+							setUsers(auth.usersObj);
+						})
+						.catch(err => toast.error('AN error occured.'));
+				} else {
+					toast.error('An error occured.');
+				}
+			})
+			.catch(err => toast.error(err.response.data.message));
+	};
 
 	const columns = [
 		{
@@ -49,9 +96,57 @@ const AdminUsers = () => {
 	return (
 		<Flex justify="center" align="center" wrap="no-wrap">
 			<Container maxW="xl" mt="5vh" centerContent>
+				<Drawer isOpen={isOpen} placement="right" initialFocusRef={firstField} onClose={onClose}>
+					<DrawerOverlay />
+					<DrawerContent>
+						<DrawerCloseButton />
+						<DrawerHeader borderBottomWidth="1px">Add New User</DrawerHeader>
+
+						<DrawerBody>
+							<Stack spacing="24px">
+								<FormControl isRequired>
+									<FormLabel htmlFor="username">Email address</FormLabel>
+									<Input
+										ref={firstField}
+										value={email}
+										onChange={e => setEmail(e.target.value)}
+										id="email"
+										type="email"
+										placeholder="temp@gmail.com"
+									/>
+									<FormHelperText>We will not share the email.</FormHelperText>
+								</FormControl>
+								<FormControl isRequired>
+									<FormLabel htmlFor="bal">Balance</FormLabel>
+									<Input
+										value={bal}
+										onChange={e => setBal(e.target.value)}
+										type="number"
+										id="bal"
+										placeholder="Enter initial balance"
+									/>
+								</FormControl>
+							</Stack>
+						</DrawerBody>
+
+						<DrawerFooter borderTopWidth="1px">
+							<Button variant="outline" mr={3} onClick={onClose}>
+								Cancel
+							</Button>
+							<Button colorScheme="blue" onClick={onFormSubmit}>
+								Submit
+							</Button>
+						</DrawerFooter>
+					</DrawerContent>
+				</Drawer>
 				<Box padding="10" minWidth="50vw" borderWidth="5" bg="#ffffff" borderRadius={10}>
 					<Center>
 						<Text fontSize="3xl">List of all Users</Text>
+					</Center>
+					<Center>
+						<Button marginTop={5} leftIcon={<GrAdd />} colorScheme="blue" onClick={onOpen}>
+							Create user
+						</Button>
 					</Center>
 					<Divider paddingTop="10" />
 
