008000ace62a803a0b048dd4134369b3d1b3afd8
Ansh Mehta <8anshmehta@gmail.com>
2021-07-22 19:36:54 +0530

    baseURL changed to prod

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//axios.defaults.baseURL = `https://salt-task-backend.herokuapp.com`;
-axios.defaults.baseURL = `http://localhost:5000`;
+axios.defaults.baseURL = `https://salt-task-backend.herokuapp.com`;
+
 const api = axios;
 
 export default api;
